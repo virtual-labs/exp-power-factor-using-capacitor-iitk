1f65d63e32479556b88193a3320149d9c9335fcb
Sneha987pbc <98075130+Sneha987pbc@users.noreply.github.com>
2023-02-20 17:13:59 +0530

    Update main.js

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,48 @@
 //Your JavaScript goes in here
+function rshift(){
+    document.getElementById("capacitor").style.transitionDuration = "2s";
+    document.getElementById("capacitor").style.transform="translate(77px, 7px)";
+    // document.getElementById("capacitor").style.transform="translateY(7px)";
+    document.getElementById("line").style.display = "none";
+}
+function lshift(){
+    document.getElementById("capacitor").style.transitionDuration = "2s";
+    document.getElementById("capacitor").style.transform="translateX(-1px)";
+    // document.getElementById("capacitor").style.transform="translateY(7px)";
+    document.getElementById("line").style.display = "block";
+}
+
+const image = document.querySelector("#switch1");
+const rotateBtn = document.querySelector("#switch1btn");
+const textBtn = document.querySelector("#switch1text");
+
+let rotation = 0;
+let on = 0;
+
+
+ function switchonoff() {
+  if(on==0){
+   rotation = 35;
+   //   image.style.transform=`translate(0,10)`;
+
+
+   image.style.transform = `rotate(${rotation}deg)`;
+   image.style.transformOrigin = "221.8px 125.2px";
+
+  
+   document.getElementById("switch1btn").style.fill = "green";
+   document.getElementById("switch1text").innerHTML =  "Off";
+   
+  on =1;
+
+
+  }
+  else if(on==1){
+    rotateBtn.style.fill="red";
+    image.style.transform = `rotate(${-rotation}deg)`;
+   document.getElementById("switch1text").innerHTML =  "On";
+   on =0;
+  }
+ 
+
+}
